a52a7ca154f5eb3f8bc04f0c39065e48df53ac1d
mohnoor94 <moh.noor94@gmail.com>
2023-09-02 20:51:40 +0300

    feat: add toast and create store api

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import {ClerkProvider} from '@clerk/nextjs'
 import {ModalProvider} from "@/providers/modal-provider";
+import {ToastProvider} from "@/providers/toast-provider";
 
 
 const inter = Inter({subsets: ['latin']})
@@ -19,6 +20,7 @@ export default function RootLayout({children}: {
 		<ClerkProvider>
 			<html lang="en">
 			<body className={inter.className}>
+			<ToastProvider/>
 			<ModalProvider/>
 			{children}
 			</body>
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -8,6 +8,9 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
+import {useState} from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
 	name: z.string().min(3)
@@ -15,6 +18,7 @@ const formSchema = z.object({
 
 export const StoreModal = () => {
 	const storeModal = useStoreModal();
+	const [loading, setLoading] = useState(false);
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
@@ -23,9 +27,17 @@ export const StoreModal = () => {
 		},
 	});
 
-	const onSubmit = (data: z.infer<typeof formSchema>) => {
-		console.log(data);
-		// TODO: Create Store
+	const onSubmit = async (data: z.infer<typeof formSchema>) => {
+		try {
+			setLoading(true);
+			const response = await axios.post("/api/stores", data);
+
+			toast.success("Store created successfully");
+		} catch (error) {
+			toast.error("Something went wrong");
+		} finally {
+			setLoading(false);
+		}
 	};
 
 	return (
@@ -46,20 +58,33 @@ export const StoreModal = () => {
 									<FormItem>
 										<FormLabel>Name</FormLabel>
 										<FormControl>
-											<Input placeholder="Joy Store" {...field} />
+											<Input
+												disabled={loading}
+												placeholder="Joy Store"
+												{...field}
+											/>
 										</FormControl>
 										<FormMessage/>
 									</FormItem>
 								)}
 							/>
 							<div className="pt-6 space-x-2 flex items-center justify-end w-full">
-							<Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-							<Button type="submit">Continue</Button>
+								<Button
+									disabled={loading}
+									variant="outline"
+									onClick={storeModal.onClose}>
+									Cancel
+								</Button>
+								<Button
+									disabled={loading}
+									type="submit">
+									Continue
+								</Button>
 							</div>
-					</form>
-				</Form>
+						</form>
+					</Form>
+				</div>
 			</div>
-		</div>
-</Modal>
-)
+		</Modal>
+	)
 }
